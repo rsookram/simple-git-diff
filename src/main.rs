bc6b8fb1ec8b69ecd3188e01839de6e153c7457b
rsookram <rashadsookram@gmail.com>
2020-05-04 20:57:59 -0400

    Explicitly lock stdout before writing

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ mod state;
 use state::State;
 use std::io;
 use std::io::BufRead;
+use std::io::Write;
 
 fn main() {
     let stdin = io::stdin();
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
 
     stdin
         .lock()
         .lines()
         .filter_map(|line| line.ok().and_then(State::next))
-        .for_each(|line| println!("{}", line));
+        .try_for_each(|line| writeln!(stdout, "{}", line))
+        .unwrap();
 }
