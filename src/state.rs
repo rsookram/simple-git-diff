e092824bbbd4f17a727b6c888bee1ae7bf15476b
rsookram <rashadsookram@gmail.com>
2020-05-06 19:45:35 -0400

    Create enum for line type

--- src/state.rs
+++ src/state.rs
@@ -2,68 +2,72 @@ pub struct State {}
 
 impl State {
     pub fn next(mut line: String) -> Option<String> {
-        if is_diff(&line) {
-            line.clear();
-            Some(line)
-        } else if is_index(&line) {
-            Some(horizontal_rule())
-        } else if is_old_filepath(&line) {
-            Some(line.replacen('-', " ", 1))
-        } else if is_new_filepath(&line) {
-            let mut line = line.replacen('+', " ", 1);
+        match Line::type_of(&line) {
+            Line::Diff => {
+                line.clear();
+                Some(line)
+            }
+            Line::Index => Some(horizontal_rule()),
+            Line::OldFilepath => Some(line.replacen('-', " ", 1)),
+            Line::NewFilepath => {
+                let mut line = line.replacen('+', " ", 1);
 
-            line.push_str("\n");
-            line.push_str(&horizontal_rule());
+                line.push_str("\n");
+                line.push_str(&horizontal_rule());
 
-            Some(line)
-        } else if is_binary_files_differ(&line) {
-            line.push_str("\n");
-            line.push_str(&horizontal_rule());
-            Some(line)
-        } else if is_addition(&line) {
-            Some(line[6..].to_string())
-        } else if is_removal(&line) {
-            let line = line.replacen('-', "", 1);
+                Some(line)
+            }
+            Line::BinaryFilesDiffer => {
+                line.push_str("\n");
+                line.push_str(&horizontal_rule());
+                Some(line)
+            }
+            Line::Addition => Some(line[6..].to_string()),
+            Line::Removal => {
+                let line = line.replacen('-', "", 1);
 
-            Some(line)
-        } else if is_context(&line) {
-            Some(line[1..].to_string())
-        } else {
-            Some(line)
+                Some(line)
+            }
+            Line::Context => Some(line[1..].to_string()),
+            Line::Other => Some(line),
         }
     }
 }
 
-fn is_diff(line: &str) -> bool {
-    line.starts_with("\x1B[1mdiff ")
+enum Line {
+    Diff,
+    Index,
+    OldFilepath,
+    NewFilepath,
+    BinaryFilesDiffer,
+    Addition,
+    Removal,
+    Context,
+    Other,
 }
 
-fn is_index(line: &str) -> bool {
-    line.starts_with("\x1B[1mindex ")
-}
-
-fn is_old_filepath(line: &str) -> bool {
-    line.starts_with("\x1B[1m--- ")
-}
-
-fn is_new_filepath(line: &str) -> bool {
-    line.starts_with("\x1B[1m+++ ")
-}
-
-fn is_binary_files_differ(line: &str) -> bool {
-    line.starts_with("Binary files ") && line.ends_with(" differ")
-}
-
-fn is_addition(line: &str) -> bool {
-    line.starts_with("\x1B[32m+")
-}
-
-fn is_removal(line: &str) -> bool {
-    line.starts_with("\x1B[31m-")
-}
-
-fn is_context(line: &str) -> bool {
-    line.starts_with(' ')
+impl Line {
+    fn type_of(line: &str) -> Self {
+        if line.starts_with("\x1B[1mdiff ") {
+            Line::Diff
+        } else if line.starts_with("\x1B[1mindex ") {
+            Line::Index
+        } else if line.starts_with("\x1B[1m--- ") {
+            Line::OldFilepath
+        } else if line.starts_with("\x1B[1m+++ ") {
+            Line::NewFilepath
+        } else if line.starts_with("Binary files ") && line.ends_with(" differ") {
+            Line::BinaryFilesDiffer
+        } else if line.starts_with("\x1B[32m+") {
+            Line::Addition
+        } else if line.starts_with("\x1B[31m-") {
+            Line::Removal
+        } else if line.starts_with(' ') {
+            Line::Context
+        } else {
+            Line::Other
+        }
+    }
 }
 
 fn horizontal_rule() -> String {
